60f2f902b45b20abecb4fe30f95b6490f2cf2a75
Pulkit077 <pulkitagarwal2k1@gmail.com>
2021-07-18 09:31:36 +0530

    rosy: wifi: add WCNSS_qcom_wlan_nv.bin
     - fixes connectivity and range issues for some users on 40MHz channel width
     - remove existing symlink for WCNSS_qcom_wlan_nv.bin
    
    Signed-off-by: Pulkit077 <pulkitagarwal2k1@gmail.com>

--- Android.mk
+++ Android.mk
@@ -91,13 +91,6 @@ $(WCNSS_INI_SYMLINK): $(LOCAL_INSTALLED_MODULE)
 	      @rm -rf $@
 	      $(hide) ln -sf /vendor/etc/wifi/$(notdir $@) $@
 
-WCNSS_BIN_SYMLINK := $(TARGET_OUT_VENDOR)/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin
-$(WCNSS_BIN_SYMLINK): $(LOCAL_INSTALLED_MODULE)
-	      @echo "WCNSS bin link: $@"
-	      @mkdir -p $(dir $@)
-	      @rm -rf $@
-	      $(hide) ln -sf /mnt/vendor/persist/$(notdir $@) $@
-
 WCNSS_DAT_SYMLINK := $(TARGET_OUT_VENDOR)/firmware/wlan/prima/WCNSS_wlan_dictionary.dat
 $(WCNSS_DAT_SYMLINK): $(LOCAL_INSTALLED_MODULE)
 	      @echo "WCNSS dat link: $@"
@@ -105,7 +98,7 @@ $(WCNSS_DAT_SYMLINK): $(LOCAL_INSTALLED_MODULE)
 	      @rm -rf $@
 	      $(hide) ln -sf /mnt/vendor/persist/$(notdir $@) $@
 
-ALL_DEFAULT_INSTALLED_MODULES += $(WCNSS_INI_SYMLINK) $(WCNSS_BIN_SYMLINK) $(WCNSS_DAT_SYMLINK)
+ALL_DEFAULT_INSTALLED_MODULES += $(WCNSS_INI_SYMLINK) $(WCNSS_DAT_SYMLINK)
 
 EGL_LIBS := libEGL_adreno.so libGLESv2_adreno.so libq3dtools_adreno.so
 EGL_32_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/lib/,$(notdir $(EGL_LIBS)))
